a4c91515eddd1af6641810749556e25254ee5725
Veronica Wharton <wharton.veronica@gmail.com>
2016-11-29 23:35:51 -0500

    Small fix

--- app/suggestion/views.py
+++ app/suggestion/views.py
@@ -73,7 +73,7 @@ def suggest_create():
 
     descriptors = Descriptor.query.all()
     for descriptor in descriptors:
-        if descriptor.values:  # Fields for option descriptors.
+        if descriptor.is_option_descriptor:  # Fields for option descriptors.
             choices = [(str(i), v) for i, v in enumerate(descriptor.values)]
             setattr(ResourceSuggestionForm,
                     descriptor.name,
