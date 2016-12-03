c3778ceac3bdc6a2cd0e0243e437c66fe187ac07
Hana Pearlman <hanapearlman@levyvlan502.2413.wlan.wireless-resnet.upenn.edu>
2016-12-03 11:14:43 -0500

    added geolocation to map

--- app/assets/scripts/asylum/main.js
+++ app/assets/scripts/asylum/main.js
@@ -114,6 +114,30 @@ function initMap() {
       });
     }
   });
+  console.log("Hi")
+  if (navigator.geolocation) {
+    console.log("Hi")
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos)
+        infoWindow.setContent('Location found.');
+        map.setCenter(pos);
+    }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+   infoWindow.setPosition(pos);
+   infoWindow.setContent(browserHasGeolocation ?
+                         'Error: The Geolocation service failed.' :
+                         'Error: Your browser doesn\'t support geolocation.');
 }
 
 $(document).ready(function(){
