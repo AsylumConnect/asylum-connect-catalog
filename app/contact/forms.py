bc02dc722913a3fb30baba6e515ba58eeba4755c
Yoni Nachmany <yoninachmany@gmail.com>
2016-12-05 19:50:17 -0500

    manage format'

--- app/contact/forms.py
+++ app/contact/forms.py
@@ -1,45 +1,43 @@
 from flask.ext.wtf import Form
-from wtforms.fields import (
-    StringField,
-    SelectField,
-    TextField,
-    TextAreaField,
-    SubmitField,
-)
+from wtforms.fields import (SelectField, StringField, SubmitField,
+                            TextAreaField, TextField)
 from wtforms.fields.html5 import EmailField
-from wtforms.validators import (
-    InputRequired,
-    Email,
-    Length,
-)
+from wtforms.validators import Email, InputRequired, Length
+
 
 class ContactForm(Form):
-    name = StringField('Name', validators=[
-        InputRequired(),
-        Length(1, 128),
-    ])
-    email = EmailField('Email', validators=[
-        InputRequired(),
-        Length(1, 64),
-        Email(),
-    ])
-    message = TextAreaField('Message', validators=[
-        InputRequired(),
-        Length(1, 1024),
-    ])
+    name = StringField(
+        'Name', validators=[
+            InputRequired(),
+            Length(1, 128),
+        ])
+    email = EmailField(
+        'Email', validators=[
+            InputRequired(),
+            Length(1, 64),
+            Email(),
+        ])
+    message = TextAreaField(
+        'Message', validators=[
+            InputRequired(),
+            Length(1, 1024),
+        ])
     submit = SubmitField('Submit')
 
+
 class ContactCategoryForm(Form):
-    name = StringField('Name', validators=[
-        InputRequired(),
-        Length(1, 128),
-    ])
+    name = StringField(
+        'Name', validators=[
+            InputRequired(),
+            Length(1, 128),
+        ])
     submit = SubmitField('Add Category')
 
-class EditCategoryNameForm(Form):
-    name = TextField('Name', validators=[
-        InputRequired(),
-        Length(1, 128),
-    ])
-    submit = SubmitField('Update name')
 
+class EditCategoryNameForm(Form):
+    name = TextField(
+        'Name', validators=[
+            InputRequired(),
+            Length(1, 128),
+        ])
+    submit = SubmitField('Update name')
