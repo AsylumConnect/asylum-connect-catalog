3ffdcc76ee0a9d2fc1b8be326d340944128a773c
Hana Pearlman <hanapearlman@seas00808.wlan.wireless-pennnet.upenn.edu>
2016-12-07 13:12:35 -0500

    Included multiple options in add_seattle_data

--- app/models/resource.py
+++ app/models/resource.py
@@ -263,7 +263,7 @@ class Resource(ResourceBase):
                 address=address,
                 latitude=doc['lat'],
                 longitude=doc['long'])
-
+            print doc['name'];
             description_association = TextAssociation(
                 text=doc['description'], descriptor=description_descriptor)
             resource.text_descriptors.append(description_association)
@@ -321,26 +321,28 @@ class Resource(ResourceBase):
 
             city = doc['city']
 
-            first_category = categories[0]
-            category_association = OptionAssociation(
-                descriptor=category_descriptor,
-                option=category_descriptor.values.index(first_category))
-            resource.option_descriptors.append(category_association)
+            if categories:
+                for category in categories:
+                    category_association = OptionAssociation(
+                        descriptor=category_descriptor,
+                        option=category_descriptor.values.index(category))
+                    resource.option_descriptors.append(category_association)
 
             if supercategories:
-                first_supercategory = supercategories[0]
-                supercategory_association = OptionAssociation(
-                    descriptor=supercategory_descriptor,
-                    option=supercategory_descriptor.values.index(
-                        first_supercategory))
-                resource.option_descriptors.append(supercategory_association)
+                for supercategory in supercategories:
+                    supercategory_association = OptionAssociation(
+                        descriptor=supercategory_descriptor,
+                        option=supercategory_descriptor.values.index(
+                            supercategory))
+                    resource.option_descriptors.append(supercategory_association)
 
             if features:
-                first_feature = features[0]
-                feature_association = OptionAssociation(
-                    descriptor=feature_descriptor,
-                    option=feature_descriptor.values.index(first_feature))
-                resource.option_descriptors.append(feature_association)
+                for feature in features:
+                    feature_association = OptionAssociation(
+                        descriptor=feature_descriptor,
+                        option=feature_descriptor.values.index(feature))
+                    resource.option_descriptors.append(feature_association)
+                print features
 
             if city:
                 city_association = OptionAssociation(
