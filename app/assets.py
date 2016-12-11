25dbfb66ba561864df5b526395b8b8114bd341e8
Yoni Nachmany <yoninachmany@gmail.com>
2016-12-10 19:04:47 -0500

    format

--- app/assets.py
+++ app/assets.py
@@ -2,11 +2,16 @@ from flask.ext.assets import Bundle
 
 app_css = Bundle('*.scss', filters='scss', output='styles/app.css')
 
-app_js = Bundle('vendor/jquery.min.js', 'app.js', filters='jsmin', output='scripts/app.js')
+app_js = Bundle(
+    'vendor/jquery.min.js', 'app.js', filters='jsmin', output='scripts/app.js')
 
 vendor_css = Bundle('vendor/*.css', output='styles/vendor.css')
 
-vendor_js = Bundle('vendor/jquery.min.js', 'vendor/*.js', filters='jsmin', output='scripts/vendor.js')
+vendor_js = Bundle(
+    'vendor/jquery.min.js',
+    'vendor/*.js',
+    filters='jsmin',
+    output='scripts/vendor.js')
 
 asylum_scss = Bundle('asylum/main.css', output='styles/asylum.css')
 
@@ -17,4 +22,8 @@ asylum_css = Bundle(
     'asylum/main-page.css',
     output='styles/asylum2.css')
 
-asylum_js = Bundle('vendor/jquery.min.js', 'asylum/*.js', filters='jsmin', output='scripts/asylum.js')
+asylum_js = Bundle(
+    'vendor/jquery.min.js',
+    'asylum/*.js',
+    filters='jsmin',
+    output='scripts/asylum.js')
