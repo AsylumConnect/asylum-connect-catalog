418c8e17c85fa236184cc5770b1db3869de106d3
Yoni Nachmany <yoninachmany@gmail.com>
2016-12-10 22:53:44 -0500

    imports

--- app/single_resource/views.py
+++ app/single_resource/views.py
@@ -1,8 +1,7 @@
 from flask import abort, flash, redirect, render_template, request, url_for
 from flask.ext.login import login_required
-from flask_wtf.file import InputRequired
 from sqlalchemy.exc import IntegrityError
-from wtforms.fields import SelectField, SelectMultipleField, TextAreaField
+from wtforms.fields import SelectMultipleField, TextAreaField
 
 from . import single_resource
 from .. import db
