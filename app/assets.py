b91399591ec6653cc2da8268c59893810408bf63
yoninachmany <yoninachmany@gmail.com>
2017-02-15 12:51:22 -0500

    shortened assets

--- app/assets.py
+++ app/assets.py
@@ -9,14 +9,7 @@ vendor_css = Bundle('vendor/*.css', output='styles/vendor.css')
 # Need to specify vendor order or JS errors on heroku
 vendor_js = Bundle(
     'vendor/jquery.min.js',
-    'vendor/semantic.min.js',
-    'vendor/tablesort.min.js',
-    'vendor/address-autocomplete.js',
-    'vendor/async.js',
-    'vendor/handlebars-v4.0.5.js',
-    'vendor/oms.min.js',
-    'vendor/papaparse.min.js',
-    'vendor/zxcvbn.js',
+    'vendor/*.js',
     filters='jsmin',
     output='scripts/vendor.js')
 
