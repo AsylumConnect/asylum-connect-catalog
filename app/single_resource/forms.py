4b068647a97da87e04c6ceeefdff38fc2b78c7ff
yoninachmany <yoninachmany@gmail.com>
2017-02-19 19:47:50 -0500

    lat, lon in single_resource form

--- app/single_resource/forms.py
+++ app/single_resource/forms.py
@@ -1,7 +1,9 @@
-from wtforms.fields import SubmitField
+from wtforms.fields import SubmitField, StringField
 
 from ..suggestion.forms import ResourceForm
 
 
 class SingleResourceForm(ResourceForm):
+    latitude = StringField('Latitude', validators=[])
+    longitude = StringField('Longitude', validators=[])
     submit = SubmitField('Save Resource')
