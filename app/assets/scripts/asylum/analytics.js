7b4ac2771119b5c8350a56685e982c804dcbda93
Tiffany Lu <tiffany.lu@appboy.com>
2017-04-19 21:28:23 -0400

    add print and feedback

--- app/assets/scripts/asylum/analytics.js
+++ app/assets/scripts/asylum/analytics.js
@@ -1,28 +1,47 @@
 function initAnalytics() {
   trackSearch();
+  trackResources();
+  $('#print').click(function() {
+    logEvent('Print', 'Print - Resources');
+  });
+  $('.feedback').click(function() {
+    logEvent('Feedback', 'Feedback - Click');
+  })
 }
 
 function trackSearch() {
   // Categories
   $('.checkbox-category:checkbox').change(function(){
     if (this.checked) {
-      ga('send', 'event', 'Search', 'Category', $(this).attr('data-analytics-label'));
+      logEvent('Search', 'Category', $(this).attr('data-analytics-label'));
     }
   });
-  
 
+  $('.btn-category').click(function(){
+    if (!$(this).hasClass('active')) {
+      logEvent('Search', 'Category', $(this).attr('data-analytics-label'));
+    }
+  });
 
   // Features
   $('.feature').click(function() {
     if (!$(this).hasClass('active')) {
-      ga('send', 'event', 'Search', 'Feature', $(this).attr('data-analytics-label'));
+      logEvent('Search', 'Feature', $(this).attr('data-analytics-label'));
     }
   });
 
   // Required Documents
   $('.checkbox-requirement:checkbox').change(function(){
     if (this.checked) {
-      ga('send', 'event', 'Search', 'Required Documents', $(this).attr('data-analytics-label'));
+      logEvent('Search', 'Required Documents', $(this).attr('data-analytics-label'));
     }
   });
+}
+
+function trackResources() {
+
+}
+
+function logEvent(category, action, label) {
+  ga('send', 'event', category, action, label);
 }
